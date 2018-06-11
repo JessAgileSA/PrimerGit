8cf2f7b444b3269556c86041d1053627a6fbecf4
Jesus Sangueza (Jess) <jess.sangueza@gmail.com>
2018-06-11 14:55:20 -0400

    Primer commit de la clase student

---
+++ Student.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace StudentsApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    /// <summary>
+    /// Student Class.
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Student ID 
+        /// </summary>
+        public int StudentId { get; set; }
+
+        /// <summary>
+        /// Student Type [Kinde|Elementary|High|University]
+        /// </summary>
+        public TypeStudent StudentType { get; set; }
+
+        /// <summary>
+        /// Student's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Student's gender.
+        /// </summary>
+        public char Gender { get; set; }
+
+        /// <summary>
+        /// TimeStamp of the record.
+        /// </summary>
+        public string TimeStamp { get; set; }
+    }
+}
